3e9e003012799cf5a62a41e37bb232bc2786418d
Kateryna2882 <govoryhina82@gmail.com>
2024-01-29 20:42:05 +0200

    Initial commit

---
+++ src/main/java/com/example/apiordermanagementsystem/ApiOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiordermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrderManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiOrderManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiordermanagementsystem/ApiOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiordermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrderManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
